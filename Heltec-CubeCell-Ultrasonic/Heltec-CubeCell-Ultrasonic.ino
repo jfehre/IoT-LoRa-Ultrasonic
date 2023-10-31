1ba44f237a13a8872b8102d19d187da19ec0e2a4
Jonas Fehrenbach <Jonas.Fehrenbach@synolus.com>
2023-10-31 16:34:40 +0100

    implement heltec-cubecell code

--- Heltec-CubeCell-Ultrasonic/Heltec-CubeCell-Ultrasonic.ino
+++ Heltec-CubeCell-Ultrasonic/Heltec-CubeCell-Ultrasonic.ino
@@ -1,15 +1,252 @@
-
 /*
- * Setup fupnction. Will be runed after each deep sleep and at start
+ * 
+ * 
+ * Codesnippets:
+ *   - Heltec CubeCell examples LoRaWAN/CayenneLPP 
+ *     https://github.com/HelTecAutomation/CubeCell-Arduino/tree/master/libraries/LoRa/examples/LoRaWAN/LoRaWan_CayenneLPP
  */
-void setup() {
-  Serial.begin(115200);
-  Serial.print("Hello World");
+
+#include <softSerial.h>
+#include "LoRaWan_APP.h"
+#include <CayenneLPP.h>
+
+// COPY the config.h.template and RENAME to config.h file in the same folder WITH YOUR TTN KEYS AND ADDR.
+#include "config.h"
+
+//sensordata
+float distance = -1;
+
+/*LoraWan channelsmask, default channels 0-7*/ 
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = LORAWAN_CLASS;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = TX_INTERVAL * 1000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = LORAWAN_NETMODE;
+
+/*ADR enable*/
+bool loraWanAdr = LORAWAN_ADR;
+
+/* set LORAWAN_Net_Reserve ON, the node could save the network info to flash, when node reset not need to join again */
+bool keepNet = LORAWAN_NET_RESERVE;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = LORAWAN_UPLINKMODE;
+
+/* Application port */
+uint8_t appPort = 2;
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 4;
+
+//Define Serial for UART connection
+softSerial mySerial(TX_PIN, RX_PIN);
+unsigned char data[4]={};
+boolean newData = false;
+boolean headerReceived = false;
+byte dataIndex = 0;
+
+/* Prepares the payload of the frame */
+static void prepareTxFrame( uint8_t port )
+{
+  /*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
+  *appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
+  *if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
+  *if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
+  *for example, if use REGION_CN470, 
+  *the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
+  */
+    CayenneLPP lpp(LORAWAN_APP_DATA_MAX_SIZE);
+    lpp.addLuminosity(1, distance);
+    lpp.getBuffer(), 
+    appDataSize = lpp.getSize();
+    memcpy(appData,lpp.getBuffer(),appDataSize);
 }
 
-/*
- * Loop is checking if we can go to deep sleep after sending data to TTN
- */
-void loop() {
+///////////////////////////////////////////////////
+// Function to read distance from different sensortypes (float distance saves distance in mm)
 
+void getDistance() {
+
+  if(ENABLE_PWR_PIN)
+  {
+    digitalWrite(PWR_PIN, LOW);
+    delay(1000);
+  }
+  
+  switch (SENSOR)
+  {
+    //read sensor over UART (sensortype A0YYUW)
+    case A0YYUW:
+    {
+      mySerial.begin(9600);
+      // delay so serial is available after begin
+      delay(100);
+      // flushing otherwise wrong data come in
+      mySerial.flush();
+      // delay so serial available after flush
+      delay(100);
+      while (mySerial.available() > 0 && newData == false) {
+        if (headerReceived == false)
+        {
+          // wait for header byte (starting with 0xFF)
+          if (mySerial.read() == 0xFF)
+          {
+            headerReceived = true;
+            data[0] = 0xFF;
+            dataIndex++;
+          }
+        }else 
+        {
+          // read data
+          data[dataIndex] = mySerial.read();
+          dataIndex++;
+          if (dataIndex == 4)
+          {
+            dataIndex = 0;
+            newData = true;
+          }
+        }
+      }
+      //calculate distance
+      int sum;
+      sum=(data[0]+data[1]+data[2])&0x00FF;
+      if(sum==data[3])
+      {
+        distance=(data[1]<<8)+data[2];
+        Serial.println(distance/10);
+      }else Serial.println("ERROR");
+    
+      newData = false;
+      headerReceived = false;
+      // end serial to avoid power consumption in sleep mode
+      mySerial.end();
+      break;
+    }
+    //read sensor over TRIG/ECHO (sensortype ex. AJ-SR04M or HC-SR04)  
+    case TRIG_ECHO:
+    {
+      digitalWrite(TRIG_PIN, LOW);  
+      delayMicroseconds(5); 
+  
+      digitalWrite(TRIG_PIN, HIGH);
+      delayMicroseconds(10); 
+  
+      digitalWrite(TRIG_PIN, LOW);
+      long duration = pulseIn(ECHO_PIN, HIGH);
+      distance = duration * 0.34/2;
+      break;
+    }
+    //default case
+    default:
+    {
+      Serial.println("ERROR: No valid sensortype");
+    }   
+  }
+
+  //stop power to sensor
+  if(ENABLE_PWR_PIN)
+  {
+    digitalWrite(PWR_PIN, HIGH);
+  }
+  
+  //print output of sensor in cm
+  Serial.print("********** Ultrasonic Distance: ");
+  Serial.print(distance / 10);
+  Serial.println(" cm"); 
+}
+
+///////////////////////////////////////////////////
+void setup() {
+  Serial.begin(115200);
+#if(AT_SUPPORT)
+  enableAt();
+#endif
+  deviceState = DEVICE_STATE_INIT;
+  LoRaWAN.ifskipjoin();
+
+  //activate pins
+  if(ENABLE_PWR_PIN)
+  {
+    pinMode(PWR_PIN, OUTPUT);
+  }
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop()
+{
+  switch( deviceState )
+  {
+    case DEVICE_STATE_INIT:
+    {
+#if(LORAWAN_DEVEUI_AUTO)
+      LoRaWAN.generateDeveuiByChipID();
+#endif
+#if(AT_SUPPORT)
+      getDevParam();
+#endif
+      printDevParam();
+      LoRaWAN.init(loraWanClass,loraWanRegion);
+      deviceState = DEVICE_STATE_JOIN;
+      break;
+    }
+    case DEVICE_STATE_JOIN:
+    {
+      LoRaWAN.join();
+      break;
+    }
+    case DEVICE_STATE_SEND:
+    {
+      //Read sensor
+      getDistance();
+      prepareTxFrame( appPort );
+      LoRaWAN.send();
+      deviceState = DEVICE_STATE_CYCLE;
+      break;
+    }
+    case DEVICE_STATE_CYCLE:
+    {
+      // Schedule next packet transmission
+      txDutyCycleTime = appTxDutyCycle + randr( 0, APP_TX_DUTYCYCLE_RND );
+      LoRaWAN.cycle(txDutyCycleTime);
+      deviceState = DEVICE_STATE_SLEEP;
+      break;
+    }
+    case DEVICE_STATE_SLEEP:
+    {
+      LoRaWAN.sleep();
+      break;
+    }
+    default:
+    {
+      deviceState = DEVICE_STATE_INIT;
+      break;
+    }
+  }
 }
--- Heltec-CubeCell-Ultrasonic/README.md
+++ Heltec-CubeCell-Ultrasonic/README.md
@@ -8,19 +8,43 @@ For the backend setup please follow the instructions on the [main readme](https:
 
 ## 2. Install Libraries in Arduino IDE
 
-1. Install the [CubeCell framework](https://heltec-automation-docs.readthedocs.io/en/latest/cubecell/quick_start.html) in Arduino (Installation with Boards Manager)
-
-
-This is still under development...
+1. Install the [CubeCell framework](https://docs.heltec.org/en/node/asr650x/htcc_ab01/quick_start.html) in Arduino (Installation with Boards Manager)
 
 ## 3. Code Setup
-
+1. Copy the Template: `cp config.h.template config.h`. (This file is filled in with private data and should not be published)
+2. Insert **Application EUI** (msb), **Device EUI** (msb) and **App Key** (msb) from the **Device Overview** in your [TheThingsNetwork](cloud.thethings.network) application to the `config.h` file. (Note: use the 'C-style' not Hex)
 This is still under development...
+3. *Optional*: Change in `config.h` the way how to read sensordata (UART/A02YYUW or Trig/Echo)
+4. *Optional*: Change the pins in `config.h`
+5. *Optional*: Change cycle time in `config.h`
+6. Upload sketch to your board by using the `CubeCell-Board (HTCC AB01)` from Cube Cell
+
+If you are using [Arduino IDE](https://www.arduino.cc/en/software) i have set following parameters in **Tools**:  
+
+![Arduino Settings](images/heltec_arduino_ide_settings.png)
+
 
 ## 4. Wiring of the Ultrasonic Sensor
 
-This is still under development...
+### UART (A02YYUW)
+1. Connect `GND` of Sensor to `GND` of Board
+2. Connect VCC of Sensor to either `3V3` or `5V` (if using a power pin, which is recommended, connect it to the corresponding pin in config, default `Vext`)
+3. Connect `RX` of Sensor to PIN `GPIO1 (TX)` 
+4. Connect `TX` of Sensor to PIN `GPIO2 (RX)`
+
+[Sensor Information](https://wiki.dfrobot.com/_A02YYUW_Waterproof_Ultrasonic_Sensor_SKU_SEN0311) (The example code only works with `delay(100)`)
+
+### Echo/Trig sensors (AJ-SR04M and HC-SR04)
+1. Connect `GND` of Sensor to `GND` of Board
+2. Connect VCC of Sensor to either `3V3` or `5V` (if using a power pin, connect it to the corresponding pin, default `Vext`)
+3. Connect `Trig` of Sensor to PIN `GPIO5` 
+4. Connect `Echo` of Sensor to PIN `GPIO4`
 
 ## 5. Heltec CubeCell AB01 Pinout:
 ![ESP Pinout](images/heltec-ab01_pinout.png)
 
+## Information about Power Consumption
+- To use full capacity of deep sleep you need to supply power over VDD (3.3V). By using USB connector the powerconsumption is a lot higher
+- End all serial connections and power pins after use, so they will not drain power during consumption
+- Using Vext as Power Pin, otherwise the board will not start with battery on VDD (3.3V) and sensor connected
+- Board: active -> 30mA : deep sleep -> 3.3µA
